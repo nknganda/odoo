edbb65107190b45d42e75b590cfd8ab5ef050c7d
Bernard Kipkorir Too <bernard.too@kamoing.co.ke>
2015-11-24 10:50:36 +0300

    modify website_jquery_validation add category website

--- website_jquery_validation/__openerp__.py
+++ website_jquery_validation/__openerp__.py
@@ -19,11 +19,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Website',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website'],
 
     # always loaded
     'data': [
