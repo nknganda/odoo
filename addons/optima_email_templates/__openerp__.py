54e0727c685296ecbe0961e46682ab127d614906
Bernard Kipkorir Too <bernard.too@kamoing.co.ke>
2015-12-15 09:51:42 +0300

    change uos to uom in customized invoices

--- addons/optima_email_templates/__openerp__.py
+++ addons/optima_email_templates/__openerp__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'account', 'portal_sale'],
+    'depends': ['base', 'sale', 'account', 'portal_sale', 'website_quote'],
 
     # always loaded
     'data': [
