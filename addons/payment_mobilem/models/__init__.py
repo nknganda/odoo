5c2feccf1935d95c179dc3ae51a28b509b6c9b1c
Bernard Kipkorir Too <bernard.too@optima.co.ke>
2015-12-23 15:16:08 +0300

    remove optima modules

--- addons/payment_mobilem/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-#import controllers
-import payment_mobile_config
-import payment_mobile
-import payment_transaction
--- website_jquery_validation/models/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class website_jquery_validation(models.Model):
-#     _name = 'website_jquery_validation.website_jquery_validation'
-
-#     name = fields.Char()
